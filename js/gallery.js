506f76c4eab3f704ae2967b3f47d8ff046136779
Eng. Ali Akhtar <ali@sayeghwater.com>
2020-03-07 13:28:02 +0300

    masonry gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,14 +1,27 @@
 //Load gallery after page has been loaded (background and profile image)
 function pageLoaded() {
 	for (var i = 1; i < 19; i++) {
-		gallery = document.getElementById('galleryBelow');
-		gallery.insertAdjacentHTML('afterend',
-		    '<div class="gallery">'+
-				'<a href="img/gallery/ref/'+i+'.jpg" target="_blank">'+
-					'<img src="img/gallery/ref/'+i+'.jpg">'+
-					'<div class="view">View</div>'+
-				'</a>'+
-			'</div>'
-		);
+		var galleryContainer = document.getElementById("galleryContainer");
+
+		// <a> Tag
+		var imageLink = document.createElement("a");
+		imageLink.className = "image-link";
+		imageLink.href = "img/gallery/ref/"+i+".jpg";
+		imageLink.target = "_blank";
+
+		// <img> Tag
+		var imageSrc = document.createElement("img");
+		imageSrc.className = "image-src";
+		imageSrc.src = "img/gallery/ref/"+i+".jpg";
+
+		// <p> Tag
+		var imageView = document.createElement("p");
+		imageView.className = "image-view";
+		imageView.innerHTML  = "View / Download";
+
+		imageLink.appendChild(imageSrc);
+		imageLink.appendChild(imageView);
+
+		galleryContainer.appendChild(imageLink);
 	}
 }
