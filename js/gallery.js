db2422ec19dd77146693d60c2d6ab27d2736a6b0
Ali Akhtar Mohammed <ali@sayeghwater.com>
2020-03-19 13:05:51 +0300

    first 10 photo

--- js/gallery.js
+++ js/gallery.js
@@ -1,18 +1,18 @@
 //Load gallery after page has been loaded (background and profile image)
 function pageLoaded() {
-	for (var i = 1; i < 18; i++) {
+	for (var i = 1; i <= 10; i++) {
 		var galleryContainer = document.getElementById("galleryContainer");
 
 		// <a> Tag
 		var imageLink = document.createElement("a");
 		imageLink.className = "image-link";
-		imageLink.href = "img/gallery/ref/"+i+".jpg";
+		imageLink.href = "img/gallery/"+i+".jpg";
 		imageLink.target = "_blank";
 
 		// <img> Tag
 		var imageSrc = document.createElement("img");
 		imageSrc.className = "image-src";
-		imageSrc.src = "img/gallery/ref/"+i+".jpg";
+		imageSrc.src = "img/gallery/thumbnail/"+i+"_thumbnail.jpg";
 
 		// <p> Tag
 		var imageView = document.createElement("p");
