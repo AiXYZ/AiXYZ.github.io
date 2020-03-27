03a478e054f82974b3b708d80270f7f61dfbbde9
Ali Akhtar Mohammed <ali@sayeghwater.com>
2020-03-27 13:23:02 +0300

    delete photo

--- js/gallery.js
+++ js/gallery.js
@@ -1,18 +1,35 @@
 //Load gallery after page has been loaded (background and profile image)
 function pageLoaded() {
-	for (var i = 1; i <= 10; i++) {
+
+	var imageArray = ([
+		["1_thumbnail.jpg", "1.jpg"],
+		["2_thumbnail.jpg", "2.jpg"],
+		["3_thumbnail.jpg", "3.jpg"],
+		["4_thumbnail.jpg", "4.jpg"],
+		["5_thumbnail.jpg", "5.jpg"],
+		["6_thumbnail.jpg", "6.jpg"],
+		["7_thumbnail.jpg", "7.jpg"],
+		["8_thumbnail.jpg", "8.jpg"],
+		["9_thumbnail.jpg", "9.jpg"],
+		["10_thumbnail.jpg", "10.jpg"]
+	]);
+
+    for (var i = 0; i < imageArray.length; i++) {
+        var thumbnail = imageArray[i][0];
+        var image = imageArray[i][1];
+
 		var galleryContainer = document.getElementById("galleryContainer");
 
 		// <a> Tag
 		var imageLink = document.createElement("a");
 		imageLink.className = "image-link";
-		imageLink.href = "img/gallery/"+i+".jpg";
+		imageLink.href = "img/gallery/"+image;
 		imageLink.target = "_blank";
 
 		// <img> Tag
 		var imageSrc = document.createElement("img");
 		imageSrc.className = "image-src";
-		imageSrc.src = "img/gallery/thumbnail/"+i+"_thumbnail.jpg";
+		imageSrc.src = "img/gallery/thumbnail/"+thumbnail;
 
 		// <p> Tag
 		var imageView = document.createElement("p");
@@ -23,5 +40,6 @@ function pageLoaded() {
 		imageLink.appendChild(imageView);
 
 		galleryContainer.appendChild(imageLink);
-	}
+    }
+
 }
